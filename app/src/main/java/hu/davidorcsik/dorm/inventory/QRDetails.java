05f5dbda3a6c1af7dd972a571d4d7c232f58d304
David Orcsik <davidorcsik@protonmail.com>
2020-07-03 18:19:55 +0200

    Basic qr registration

--- app/src/main/java/hu/davidorcsik/dorm/inventory/QRDetails.java
+++ app/src/main/java/hu/davidorcsik/dorm/inventory/QRDetails.java
@@ -9,8 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class QRDetails extends Fragment {
+    private long qrId;
+    private EditText edtRoomNumber;
+    private EditText edtInventoryId;
+    private Spinner spnItemType;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,46 @@ public class QRDetails extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_qr_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        ((RegisterQRCode)view.getContext()).setDetailsFragment(this);
+        edtRoomNumber = view.findViewById(R.id.edtRoomNumber);
+        edtInventoryId = view.findViewById(R.id.edtInventoryId);
+        spnItemType = view.findViewById(R.id.spnItemType);
+        spnItemType.setAdapter(ArrayAdapter.createFromResource(view.getContext(), R.array.item_types, android.R.layout.simple_spinner_item));
+    }
+
+    public long getQrId() {
+        return qrId;
+    }
+
+    public void setQrId(long qrId) {
+        this.qrId = qrId;
+    }
+
+    public EditText getEdtRoomNumber() {
+        return edtRoomNumber;
+    }
+
+    public void setEdtRoomNumber(EditText edtRoomNumber) {
+        this.edtRoomNumber = edtRoomNumber;
+    }
+
+    public EditText getEdtInventoryId() {
+        return edtInventoryId;
+    }
+
+    public void setEdtInventoryId(EditText edtInventoryId) {
+        this.edtInventoryId = edtInventoryId;
+    }
+
+    public Spinner getSpnItemType() {
+        return spnItemType;
+    }
+
+    public void setSpnItemType(Spinner spnItemType) {
+        this.spnItemType = spnItemType;
     }
 }
--- app/src/main/java/hu/davidorcsik/dorm/inventory/RegisterQRCode.java
+++ app/src/main/java/hu/davidorcsik/dorm/inventory/RegisterQRCode.java
@@ -7,21 +7,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import hu.davidorcsik.dorm.inventory.utility.InventoryItem;
 import hu.davidorcsik.dorm.inventory.utility.QRCaptureActivity;
 
 public class RegisterQRCode extends AppCompatActivity {
+    private QRDetails detailsFragment;
 
+    protected void setDetailsFragment(QRDetails df) {
+        detailsFragment = df;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_qr_code);
 
         findViewById(R.id.btnReadQR).setOnClickListener(scanButtonClickListener);
+        findViewById(R.id.btnSave).setOnClickListener(saveButtonClickListener);
     }
 
     @Override
@@ -29,9 +39,16 @@ public class RegisterQRCode extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             String qrContent = result.getContents();
-            if (qrContent != null) Toast.makeText(this, qrContent, Toast.LENGTH_LONG).show();
-            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-            vibrator.vibrate(100);
+            if (qrContent != null) {
+                try {
+                    detailsFragment.setQrId(Long.parseLong(qrContent));
+                } catch (NumberFormatException e) {
+                    detailsFragment.setQrId(1234); //TODO: REMOVE AFTER DEBUG!!!!!
+                    Toast.makeText(RegisterQRCode.this, "Hibás QR kód!", Toast.LENGTH_LONG).show();
+                }
+                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                vibrator.vibrate(100);
+            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
@@ -41,6 +58,23 @@ public class RegisterQRCode extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             QRCaptureActivity.initQRScan(RegisterQRCode.this);
+        }
+    };
+
+    private View.OnClickListener saveButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                InventoryItem item = new InventoryItem(detailsFragment);
+                try {
+                    String jsonszar = new ObjectMapper().writeValueAsString(item);
+                    int a = 0;
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            } catch (IllegalArgumentException e) {
+                Toast.makeText(RegisterQRCode.this, "Kérleg a megfelelő mezőket töltsd ki!", Toast.LENGTH_LONG).show();
+            }
         }
     };
 }
